6d46dfe008fa8512461ccf19473213d84c8dbe4c
meso_cacase <y-naito@dbcls.rois.ac.jp>
2021-02-04 21:33:11 +0900

    RefSeq 204に更新

--- gbff.pl
+++ gbff.pl
@@ -23,7 +23,7 @@ eval 'use HTML::Template ; 1' or  # HTMLをテンプレート化
 eval 'use Sedue ; 1' or           # Sedueに問い合わせを行うためのモジュール
 	print_html('ERROR : cannot load Sedue') ;
 
-my $refseq_version = 'RefSeq release 203 (Nov, 2020)' ;
+my $refseq_version = 'RefSeq release 204 (Jan, 2021)' ;
 my $ddbj_version   = 'DDBJ release 92.0 (Feb, 2013)' ;
 
 my $timestamp = timestamp() ;     # CGIを実行した時刻
