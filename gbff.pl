d951ca352e5a8e3ddb6f208bee13dd62e747f50a
meso_cacase <y-naito@dbcls.rois.ac.jp>
2023-10-23 19:18:51 +0900

    RefSeq 220に更新

--- gbff.pl
+++ gbff.pl
@@ -23,7 +23,7 @@ eval 'use HTML::Template ; 1' or  # HTMLをテンプレート化
 eval 'use Sedue ; 1' or           # Sedueに問い合わせを行うためのモジュール
 	print_html('ERROR : cannot load Sedue') ;
 
-my $refseq_version = 'RefSeq release 219 (Jul, 2023)' ;
+my $refseq_version = 'RefSeq release 220 (Sep, 2023)' ;
 my $ddbj_version   = 'DDBJ release 92.0 (Feb, 2013)' ;
 
 my $timestamp = timestamp() ;     # CGIを実行した時刻
